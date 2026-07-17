03bb30ade10f8477a33ba1d04ab8444ec67c784f
Ludwig Cheung <iehova.hao@gmail.com>
2026-07-17 23:51:28 +0900

    Update sw.js
    Refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE="jql-v1";
+const CACHE="jql-v2";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./assets/icon-192.png","./assets/icon-512.png","./privacy.html","./terms.html"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
